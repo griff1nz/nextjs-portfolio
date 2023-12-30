6fd21246ddddcfdcd86a56b880edbbf2aca5f1ba
Zion Griffin <defendingmymaininbox@gmail.com>
2023-12-30 17:44:03 -0500

    Did some styling stuff

---
+++ app/contact/page.tsx
@@ -0,0 +1,5 @@
+export default function Contact() {
+    return (
+        <h1 className="text-5xl font-serif">Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/>Hello chum!<br/></h1>
+    )
+}
